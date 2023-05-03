57c89310094bfa14a8a38bf7a0cdf9013bb5746b
BohdanMus <bogdanmusienko@gmail.com>
2023-05-03 21:25:35 +0300

    add file

---
+++ src/components/Feedback/FeedbackOptionsStyled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const BtnList = styled.ul`
+  display: flex;
+  gap: 20px;
+  padding: 0;
+  list-style: none;
+`;
+
+export const Btn = styled.button`
+  padding: 10px 24px;
+  background-color: green;
+  color: white;
+  border: none;
+  border-radius: 16px;
+  font-weight: 500;
+  font-size: 12px;
+  text-transform: uppercase;
+`;
